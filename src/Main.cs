3ab26ea8836b4b55eeb3dc0f3d2e105fe95fe6e6
Vidyut Venkatesan <vidyut0903@gmail.com>
2026-05-11 00:08:28 +1000

    Handle Ctrl+C and sigterm

--- src/Main.cs
+++ src/Main.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Markdig;
 
 namespace MdRenderer;
@@ -6,15 +7,22 @@ public class Main
 {
     public static async Task<int> RunAsync(string filePath)
     {
+        Console.CancelKeyPress += (_, e) => e.Cancel = true;
+        using var _ = PosixSignalRegistration.Create(PosixSignal.SIGTERM, _ =>
+        {
+            Writer.CloseAlternateBuffer();
+            Environment.Exit(0);
+        });
+
         using var reader = new StreamReader(filePath);
         string content = reader.ReadToEnd();
 
         var pipeline = new MarkdownPipelineBuilder().UseEmphasisExtras().UsePipeTables().Build();
         var doc = Markdown.Parse(content, pipeline);
-        var renderer = new MdRenderer.Renderer();
+        var renderer = new Renderer();
         var lines = renderer.Render(doc, content);
 
-        var pager = new MdRenderer.Pager(lines, filePath);
+        var pager = new Pager(lines, filePath);
         pager.Run();
         
         return 0;
